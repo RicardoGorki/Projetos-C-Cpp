96d46393402457764ab278f256e274c269b471d9
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-05-24 10:58:08 -0300

    cpp

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+
+	if (argc > 1)
+	{
+		while(i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if(islower(argv[i][j]))
+					argv[i][j] -= 32;
+				std::cout << argv[i][j++];
+			}
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << "\n";
+	return (0);
+}
