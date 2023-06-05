8435dae45966548a1d5e3c24b577197ff6e5a8e9
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-06-05 15:49:37 -0300

    fix errors

--- cpp/cpp_00/ex01/Contact.hpp
+++ cpp/cpp_00/ex01/Contact.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cctype>
+#include <cstdlib>
 class Contact {
 
 private:
--- cpp/cpp_00/ex01/PhoneBook.cpp
+++ cpp/cpp_00/ex01/PhoneBook.cpp
@@ -11,35 +11,105 @@ PhoneBook::~PhoneBook()
 	std::cout << "Destructor called" << std::endl;
 }
 
+int check_onlyspace(std::string str)
+{
+	int i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	if (str.empty())
+		return (1);
+	while(str[i])
+	{
+		if (str[i] != 32 && str[i] != 10)
+			flag++;
+		i++;
+	}
+	if (flag != 0)
+		return (0);
+	return (1);
+}
+
+int check_has_space(std::string str)
+{
+	int i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	if (str.empty())
+		return (1);
+	while(str[i])
+	{
+		if ((str[i] == 32 && str[i] != 10) || isdigit(str[i]))
+			flag++;
+		i++;
+	}
+	if (flag != 0)
+		return (1);
+	return (0);
+}
+
+int my_isdigit(std::string str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9') && str[i] != ' ')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 void PhoneBook::add_contact(int index)
 {
 	int			flag;
-	std::string str;
+	std::string str[5];
 
 	flag = 1;
-	if (_count_contact < 8)
-		_count_contact++;
 	while(flag)
 	{
 		std::cout << "\033[0;31mEnter with First Name\033[0m" << std::endl;
-		std::getline(std::cin, str);
-		if (!str.empty())
-			PhoneBook::_contact[index].setFirstName(str);
-		else
-			flag = 0;
+		std::getline(std::cin, str[0]);
+		if (check_has_space(str[0]) || !my_isdigit(str[0]))
+			break ;
+
 		std::cout << "\033[0;31mEnter with Last Name\033[0m" << std::endl;
-		std::getline(std::cin, str);
-		this->_contact[index].setLastName(str);
+		std::getline(std::cin, str[1]);
+		if (check_has_space(str[1]) || !my_isdigit(str[1]))
+			break ;
+
 		std::cout << "\033[0;31mEnter with Nick Name\033[0m" << std::endl;
-		std::getline(std::cin, str);
-		this->_contact[index].setNickName(str);
+		std::getline(std::cin, str[2]);
+		if (check_onlyspace(str[2]))
+			break ;
+
 		std::cout << "\033[0;31mEnter with Phone Number\033[0m" << std::endl;
-		std::getline(std::cin, str);
-		this->_contact[index].setPhoneNumber(str);
+		std::getline(std::cin, str[3]);
+		if (check_onlyspace(str[3]) || my_isdigit(str[3]))
+			break ;
+
 		std::cout << "\033[0;31mEnter with Darkest Secret\033[0m" << std::endl;
-		std::getline(std::cin, str);
-		this->_contact[index].setDarkestSecret(str);
-		std::cout << "\033[0;32mContact registred with Success\033[0m" << std::endl;
+		std::getline(std::cin, str[4]);
+		if (check_onlyspace(str[4]))
+			break ;
+
+		if (!str[0].empty() && !str[1].empty() && !str[2].empty()
+			&& !str[3].empty() && !str[4].empty())
+		{
+			this->_contact[index].setFirstName(str[0]);
+			this->_contact[index].setLastName(str[1]);
+			this->_contact[index].setNickName(str[2]);
+			this->_contact[index].setPhoneNumber(str[3]);
+			this->_contact[index].setDarkestSecret(str[4]);
+			std::cout << "\033[0;32mContact registred with Success\033[0m" << std::endl;
+			if (_count_contact < 8)
+				_count_contact++;
+		}
 		flag = 0;
 	}
 }
