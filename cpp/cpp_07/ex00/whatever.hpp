7d8785857d3d61e3c6752337e77c8f7716fc14d3
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-08-23 19:02:12 -0300

    first

---
+++ cpp/cpp_07/ex00/whatever.hpp
@@ -0,0 +1,17 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template < typename T >
+T const & swap( T const & x, T const & y)
+{
+	T const
+}
+
+T const & min( T const & x, T const & y)
+{
+
+};
+T const & max( T const & x, T const & y)
+{
+};
+#endif
