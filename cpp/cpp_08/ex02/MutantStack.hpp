5e3d6ea24c9f8e278326719ef621785cbe87642e
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-08-25 15:54:36 -0300

    add

--- cpp/cpp_08/ex02/MutantStack.hpp
+++ cpp/cpp_08/ex02/MutantStack.hpp
@@ -1,10 +1,60 @@
 #ifndef MUTANTSTACK_HPP
-# define MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
-template<typename T>
-class MutantStack
+#include <iostream>
+#include <stack>
+#include <deque>
+
+template <class T>
+class MutantStack : public std::stack<T>
 {
+public:
+	MutantStack();
+	~MutantStack();
+	MutantStack(MutantStack<T> &other);
+	MutantStack<T> &operator=(MutantStack<T> &other);
 
+	//template <class T>
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	iterator begin();
+	iterator end();
+	//template <std::stack<T>::container_type::iterator iterator>
+
+	/* MutantStack<T>::iterator MutantStack<T>::begin()
+	{
+    	return std::stack<T>::c.begin();
+	} */
 };
+
+	template <class T>
+	typename MutantStack<T>::iterator MutantStack<T>::begin()
+	{
+		return std::stack<T>::c.begin();
+	};
+
+	template <class T>
+	typename MutantStack<T>::iterator MutantStack<T>::end()
+	{
+		return std::stack<T>::c.end();
+	};
+
+	template <class T>
+	MutantStack<T>::MutantStack(){};
+
+	template <class T>
+	MutantStack<T>::~MutantStack(){};
+
+	template <class T>
+	MutantStack<T>::MutantStack(MutantStack<T> &other)
+	{
+		(void)other;
+	};
+
+	template <class T>
+	MutantStack<T> &MutantStack<T>::operator=(MutantStack<T> &other)
+	{
+		(void)other;
+		return (*this);
+	};
 
 #endif
